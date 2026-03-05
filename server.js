b667a9680026d590bcc7de5e67ebfb114ac63018
Daniel Lopez <daloma131994@gmail.com>
2026-03-04 21:41:58 -0600

    agrego segundo commit del proyecto

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  res.write("Hola Daniel, este es mi primer servidor backend 🚀");
-  res.end();
-});
-
-server.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
-});
+console.log("Segundo cambio con Git 🚀");
